f18ab0c0d1f69461d653b1ae73a35c56f02d6528
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-03-26 11:35:34 -0300

    Connection with MongoDB

---
+++ crud_app/server/database/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    try {
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           useFindAndModify: false,
+           useCreateIndex: true 
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
