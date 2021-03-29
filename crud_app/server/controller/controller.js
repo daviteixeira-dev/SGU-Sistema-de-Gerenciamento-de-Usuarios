e3f7ff41ef10bc97dd428866dec5c422bab5c184
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-03-29 11:57:54 -0300

    Find & Update a User

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -31,12 +31,35 @@ exports.create = (req, res) => {
 
 // retrieve and return all users/ retrive and return a single user
 exports.find = (req, res) => {
-
+    Userdb.find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        res.status(500).send({ message : err.message || "Error Occurred while retriving user information"})
+    })
 }
 
 // Update a new identified user by user id
 exports.update = (req, res) => {
+    if(!req.body){
+        return res
+          .status(400)
+          .send({ message : "Data to update can not be empty"})
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+      .then(data => {
+          if(!data){
+              res.status(404).send({ message: `Cannot Update user with ${id}. Maybe user not found!`})
+          }else{
+              res.send(data)
+          }
+      })
+      .catch(err => {
+          res.status(500).send({ message : "Error Update user information"})
+      })
 }
 
 // Delete a user with specified user id in the request
