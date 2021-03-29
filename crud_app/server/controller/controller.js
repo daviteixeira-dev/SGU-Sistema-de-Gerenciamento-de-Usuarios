f154d1f2047f320df12578858d69b91327d40016
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-03-29 12:16:32 -0300

    Delete a User

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -64,5 +64,21 @@ exports.update = (req, res) => {
 
 // Delete a user with specified user id in the request
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Userdb.findByIdAndDelete(id)
+      .then(data => {
+          if(!data){
+              res.status(400).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+          }else{
+              res.send({
+                  message : "User was deleted successfully!"
+              })
+          }
+      })
+      .catch(err => {
+          res.status(500).send({
+              message: "Could not delete User with id=" + id
+          });
+      });
 }
