6a73b340f212df7335ac6257b97d398b1b41ec23
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-03-30 21:54:06 -0300

    add to heroku

--- crud_app/server.js
+++ crud_app/server.js
@@ -32,3 +32,4 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`)});
+//add to heroku
